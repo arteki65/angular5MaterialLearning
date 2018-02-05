a8456c6d18743f21b08a4fe224c30ee3a3553088
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-02-05 13:10:57 +0100

    after lecture 23

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,3 +1,10 @@
+import {NgModule} from '@angular/core';
+import {MatButton, MatButtonModule, MatIconModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule]
+})
 export class MaterialModule {
 
 }
