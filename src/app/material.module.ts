a9e19e443ac5d3e96e01aaba78a0d859a300bb42
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-02-14 14:12:40 +0100

    after lecture 33

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {
-  MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule,
+  MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule,
   MatNativeDateModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
-    MatNativeDateModule],
+    MatNativeDateModule, MatCheckboxModule],
   exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
-    MatNativeDateModule]
+    MatNativeDateModule, MatCheckboxModule]
 })
 export class MaterialModule {
 
