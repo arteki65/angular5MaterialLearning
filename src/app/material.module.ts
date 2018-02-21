011878c00b0dc59eba4528d34721ea977b7e2bfc
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-02-21 11:01:41 +0100

    after lecture 49

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
 import {
-  MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule,
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatIconModule,
+  MatInputModule, MatListModule,
   MatNativeDateModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatTabsModule, MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
     MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,
-    MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule],
+    MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule],
   exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
     MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,
-    MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule]
+    MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule]
 })
 export class MaterialModule {
 
