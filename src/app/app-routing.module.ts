279e86c36b34e06213102aa9df65a701a150e132
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-02-22 14:03:33 +0100

    after lecture 56

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
 import {TrainingComponent} from './training/training/training.component';
+import {AuthGurad} from './auth/auth.gurad';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'training', component: TrainingComponent}
+  {path: 'training', component: TrainingComponent, canActivate: [AuthGurad]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGurad]
 })
 export class AppRoutingModule {
 }
