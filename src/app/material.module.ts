62d0c57923f151ecb6dbb5091c6517baf3cb22e2
arteki65 <arteki65@gmail.com>
2018-03-05 20:42:20 +0100

    after section 5

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,20 @@ import {NgModule} from '@angular/core';
 import {
   MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatIconModule,
   MatInputModule, MatListModule,
-  MatNativeDateModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatTableModule, MatTabsModule, MatToolbarModule
+  MatNativeDateModule, MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSortModule, MatTableModule,
+  MatTabsModule,
+  MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
     MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,
     MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule,
-    MatTableModule],
+    MatTableModule, MatSortModule, MatPaginatorModule],
   exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
     MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule,
     MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule,
-    MatTableModule]
+    MatTableModule, MatSortModule, MatPaginatorModule]
 })
 export class MaterialModule {
 
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {Exercise} from '../training/exercise.model';
 import {TrainingService} from '../training/training.service';
 
@@ -8,9 +8,11 @@ import {TrainingService} from '../training/training.service';
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sortConf: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) {
   }
@@ -19,4 +21,12 @@ export class PastTrainingsComponent implements OnInit {
     this.dataSource.data = this.trainingService.getPastExercises();
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sortConf;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
 }
